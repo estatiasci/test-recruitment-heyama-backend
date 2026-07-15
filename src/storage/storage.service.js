40766628b6fc5f20dc8f2fb46fdf43654099de0b
legrandbakobogisdev <legrand.bakobog.isdev@gmail.com>
2026-07-15 09:28:50 +0100

    feat: use Cloudinary instead of Backblaze B2 for image storage

--- src/storage/storage.service.js
+++ src/storage/storage.service.js
@@ -1,10 +1,6 @@
 const { Injectable, Dependencies } = require('@nestjs/common');
 const { ConfigService } = require('@nestjs/config');
-const {
-  S3Client,
-  PutObjectCommand,
-  DeleteObjectCommand,
-} = require('@aws-sdk/client-s3');
+const cloudinary = require('cloudinary').v2;
 const { v4: uuidv4 } = require('uuid');
 
 @Injectable()
@@ -13,52 +9,41 @@ const { v4: uuidv4 } = require('uuid');
 class StorageService {
   constructor(configService) {
     this.configService = configService;
-    this.bucketName = configService.get('B2_BUCKET_NAME');
-    this.publicUrl = configService.get('B2_PUBLIC_URL');
 
-    // Backblaze B2 est compatible avec l'API S3 : on utilise le SDK AWS S3
-    // (une simple librairie cliente, aucune dépendance réelle à AWS) en
-    // pointant vers l'endpoint S3-compatible de B2.
-    this.s3Client = new S3Client({
-      region: configService.get('B2_REGION'), // ex: "us-west-002"
-      endpoint: configService.get('B2_ENDPOINT'), // ex: https://s3.us-west-002.backblazeb2.com
-      credentials: {
-        accessKeyId: configService.get('B2_KEY_ID'),
-        secretAccessKey: configService.get('B2_APPLICATION_KEY'),
-      },
+    cloudinary.config({
+      cloud_name: configService.get('CLOUDINARY_CLOUD_NAME'),
+      api_key: configService.get('CLOUDINARY_API_KEY'),
+      api_secret: configService.get('CLOUDINARY_API_SECRET'),
+    });
+
+    this.cloudinary = cloudinary;
+  }
+
+  /**
+   * Télécharge un fichier vers Cloudinary et retourne son URL publique + son public_id.
+   */
+  uploadFile(file) {
+    const publicId = `objects/${uuidv4()}`;
+
+    return new Promise((resolve, reject) => {
+      const uploadStream = this.cloudinary.uploader.upload_stream(
+        { public_id: publicId, resource_type: 'image' },
+        (error, result) => {
+          if (error) {
+            return reject(error);
+          }
+          resolve({ url: result.secure_url, key: result.public_id });
+        },
+      );
+      uploadStream.end(file.buffer);
     });
   }
 
   /**
-   * Télécharge un fichier vers le bucket B2 et retourne son URL publique + sa clé.
-   */
-  async uploadFile(file) {
-    const extension = file.originalname.split('.').pop();
-    const key = `objects/${uuidv4()}.${extension}`;
-
-    await this.s3Client.send(
-      new PutObjectCommand({
-        Bucket: this.bucketName,
-        Key: key,
-        Body: file.buffer,
-        ContentType: file.mimetype,
-      }),
-    );
-
-    const url = `${this.publicUrl}/${key}`;
-    return { url, key };
-  }
-
-  /**
-   * Supprime un fichier du bucket B2 à partir de sa clé.
+   * Supprime un fichier de Cloudinary à partir de son public_id.
    */
   async deleteFile(key) {
-    await this.s3Client.send(
-      new DeleteObjectCommand({
-        Bucket: this.bucketName,
-        Key: key,
-      }),
-    );
+    await this.cloudinary.uploader.destroy(key);
   }
 }
 
